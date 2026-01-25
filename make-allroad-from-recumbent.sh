e6915a1e4e3d0f1fda4299bf89a97511c330ebfb
Robert Voigt <robert.voigt@gmx.de>
2026-01-25 17:01:28 +0100

    update scripts

--- make-allroad-from-recumbent.sh
+++ make-allroad-from-recumbent.sh
@@ -1,1 +1,3 @@
+# make allroad-rv.brf from recumbent-bike-fast-rv.brf and the diff that has everything that's different
+
 patch recumbent-bike-fast-rv.brf recumbent-allroad.diff -o allroad-rv.brf
---
+++ make-recumbent-allroad-diff.sh
@@ -0,0 +1,3 @@
+# make recumbent-allroad.diff after one or both have been changed
+
+diff -u recumbent-bike-fast-rv.brf allroad-rv.brf > recumbent-allroad.diff
